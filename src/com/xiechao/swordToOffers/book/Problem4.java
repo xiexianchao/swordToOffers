5d842553e3cd0cfaab47c56da198d6513076d6b3
xiechao <907796939@qq.com>
2018-09-05 15:17:39 +0800

    测试

---
+++ src/com/xiechao/swordToOffers/book/Problem4.java
@@ -0,0 +1,31 @@
+package com.xiechao.swordToOffers.book;
+
+/**
+ * @author: xiehcao
+ * @Date : 2018/9/5
+ * @Time : 15:03
+ * @description :二维数组中的查找
+ */
+
+//牛客网测试 140ms
+public class Problem4 {
+    public boolean isExists(int[][] array,int target){
+        boolean result = false;
+        if( array != null && array.length >0 && array[0] !=null && array[0].length >0){
+            int j = 0;
+            int i = array.length-1;
+            while( i>=0 && j <= (array[0].length-1)){
+             if(array[i][j] == target){
+                 result = true;
+                 break;
+             }
+             if(array[i][j] > target)
+                i--;
+             else if(array[i][j] < target){
+                j++;
+             }
+            }
+        }
+        return result;
+    }
+}
