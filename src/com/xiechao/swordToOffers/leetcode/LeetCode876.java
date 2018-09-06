8eadb45769d9554ea058aaf735bc69ecd8e63d7d
xiechao <907796939@qq.com>
2018-09-06 19:56:24 +0800

    离开实验室

---
+++ src/com/xiechao/swordToOffers/leetcode/LeetCode876.java
@@ -0,0 +1,20 @@
+package com.xiechao.swordToOffers.leetcode;
+
+/**
+ * @author: xiehcao
+ * @Date : 2018/9/6
+ * @Time : 19:51
+ * @description :
+ */
+public class LeetCode876 {
+    private static class ListNode{
+        int val;
+        ListNode next;
+        ListNode(int x){this.val = val;}
+    }
+    public ListNode middleNode(ListNode head) {
+
+
+
+    }
+}
