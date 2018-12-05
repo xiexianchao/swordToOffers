fd256dafa83b251a47482a7495646ba92c020cca
xiechao <199502>
2018-12-05 20:19:51 +0800

    12月5日每日一练

---
+++ src/com/xiechao/swordToOffers/algorithms/dp/LeetCode718.java
@@ -0,0 +1,43 @@
+package com.xiechao.swordToOffers.algorithms.dp;
+
+/**
+ * @ClassName LeetCode718
+ * @Author xiechao
+ * @Date 2018/12/5
+ * @Time 10:13
+ * @Description Maximum Length of Repeated Subarray
+ * 求两个字符串的最长公共子串
+ *
+ */
+public class LeetCode718 {
+    //LeetCode 答案 62ms
+    public int findLength(int[] A, int[] B) {
+        if(A == null || B == null || A.length <= 0 || B.length <= 0) return 0;
+        int res = 0;
+        int[][] dp = new int[A.length+1][B.length+1];
+        for (int i = A.length - 1; i >= 0 ; i--) {
+            for (int j = B.length - 1; j >= 0 ; j--) {
+                if(A[i] == B[j])
+                    dp[i][j] = dp[i+1][j+1] + 1;
+                else
+                    dp[i][j] = 0;
+                res = Math.max(res,dp[i][j]);
+            }
+        }
+        return res;
+    }
+    //LeetCode大神 优化
+    public int findLength2(int[] A, int[] B) {
+        if(A == null || B == null || A.length <= 0 || B.length <= 0) return 0;
+        int res = 0;
+        int[] dp = new int[B.length + 1];
+        for (int i = A.length -1; i >= 0 ; i--) {
+            for (int j = 0; j < dp.length; j++) {
+                res = Math.max(res,dp[j] = A[i] == B[j]? 1 + dp[j+1]:0);
+            }
+        }
+        return res;
+    }
+
+
+}
